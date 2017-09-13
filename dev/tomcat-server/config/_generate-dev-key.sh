c1a45fdedfa2dd620a4d08da7f5ae794ce58b80e
Jason Harwig <jason.harwig@gmail.com>
2017-09-13 15:15:29 -0500

    Fix tomcat dev cert for Chrome 61+ (#1311)
    * New dev tomcat cert. Uses subject alt names so secure when using localhost.
    
    Fixes Chrome 61+ websockets not working
    
    Requires adding cert to "keychain access" on Mac OS and trusting it.
    
    * gen script

---
+++ dev/tomcat-server/config/_generate-dev-key.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+keytool \
+  -genkey \
+  -keyalg RSA \
+  -ext san=dns:localhost,ip:127.0.0.1,ip:::1 \
+  -alias visallo-vm.visallo.org \
+  -keystore visallo-vm.visallo.org.jks \
+  -storepass password \
+  -validity 360 \
+  -keysize 2048
