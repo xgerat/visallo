181dce6868c2d20df76f093ee14438426fdc9e69
Jason Harwig <jason.harwig@gmail.com>
2017-10-02 13:45:32 -0500

    Fix test cert/key for livereload (#1333)

--- dev/tomcat-server/config/_generate-dev-key.sh
+++ dev/tomcat-server/config/_generate-dev-key.sh
@@ -9,3 +9,23 @@ keytool \
   -storepass password \
   -validity 360 \
   -keysize 2048
+
+# For livereload in webapp/test/localhost.[cert|key]
+
+keytool -export \
+  -alias visallo-vm.visallo.org \
+  -file localhost.der \
+  -storepass password \
+  -keystore visallo-vm.visallo.org.jks
+openssl x509 -inform der -in localhost.der -out localhost.cert
+mv localhost.cert ../../../web/war/src/main/webapp/test/
+rm localhost.der
+
+keytool -importkeystore \
+  -srckeystore visallo-vm.visallo.org.jks \
+  -storepass password \
+  -destkeystore localhost.p12 \
+  -deststoretype PKCS12
+openssl pkcs12 -in localhost.p12  -nodes -nocerts -out localhost.key
+mv localhost.key ../../../web/war/src/main/webapp/test/
+rm localhost.p12
