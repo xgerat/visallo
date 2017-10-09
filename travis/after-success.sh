447211d7b148cc862dd8170c7c66310de376ffb1
Matt Wizeman <matt.wizeman@gmail.com>
2017-10-08 22:23:46 -0400

    Switch travis to use docker when building (#1352)
    * Switch travis to use docker when building
    
    * added docker login and pull before the build starts

--- travis/after-success.sh
+++ travis/after-success.sh
@@ -1,32 +1,33 @@
-#!/bin/bash
+#!/bin/bash -e
 
-set -e
-
-if [ "$BUILD_DOCS" ]; then
+if [ "${BUILD_DOCS}" ]; then
 
   if [ -d "docs/_book" ]; then
-
-    # Cloudfront is in preview for the CLI, enable it
-    aws configure set preview.cloudfront true
-
-    if [ "$TRAVIS_BRANCH" = "$VERSION_ROOT" ]; then
-      # Copy to root => docs.visallo.org/*
-      aws s3 sync docs/_book s3://docs-visallo-com --delete --exclude "versions/*"
-      # Copy to versioned => docs.visallo.org/versions/$BRANCH
-      aws s3 sync s3://docs-visallo-com "s3://docs-visallo-com/versions/$TRAVIS_BRANCH" --delete --exclude "versions/*"
-      # Invalidate cloudfront distribution (whole deployment)
-      aws cloudfront create-invalidation --distribution-id E1FQ5XMTOW2BH8 --paths "/*"
-    else
-      # Copy to versioned => docs.visallo.org/versions/$BRANCH
-      aws s3 sync docs/_book "s3://docs-visallo-com/versions/$TRAVIS_BRANCH" --delete
-      # Invalidate cloudfront distribution (just this version)
-      aws cloudfront create-invalidation --distribution-id E1FQ5XMTOW2BH8 --paths "/versions/$TRAVIS_BRANCH/*"
-    fi
+    docker run --volume $(pwd):/root/visallo \
+         -w /root/visallo \
+         -e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" \
+         -e "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" \
+         -e "TRAVIS_BRANCH=${TRAVIS_BRANCH}" \
+         -e "VERSION_ROOT=${VERSION_ROOT}" \
+         --rm -it visallo/travis:visallo-4.0.0 \
+         /bin/sh -c "travis/publish-docs.sh"
   fi
 else
-  test $TRAVIS_REPO_SLUG == "visallo/visallo" && test $TRAVIS_BRANCH == "master" || test 'echo $TRAVIS_BRANCH | grep -Eq /^release-.*$/' && test $TRAVIS_PULL_REQUEST == "false" && echo "<settings><servers><server><id>sonatype-nexus-snapshots</id><username>\${env.DEPLOY_USERNAME}</username><password>\${env.DEPLOY_PASSWORD}</password></server></servers></settings>" > ~/settings.xml
-  test -f ~/settings.xml && mvn -B --settings ~/settings.xml -f root/pom.xml deploy
-  test -f ~/settings.xml && mvn -B --settings ~/settings.xml -DskipTests deploy
-  CREATED_BY="$TRAVIS_REPO_SLUG commit "`git rev-parse --short HEAD`""
-  if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then ./travis/travis-request.sh --repo $TRAVIS_DOWNSTREAM_REPO --token $TRAVIS_ACCESS_TOKEN_PRO --by "$CREATED_BY" --branch $TRAVIS_BRANCH; fi
+  if [ ${TRAVIS_REPO_SLUG} = "visallo/visallo" ]; then
+    if [ ${TRAVIS_BRANCH} = "master" ] || echo ${TRAVIS_BRANCH} | grep -Eq '^release-.*$'; then
+      if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+        docker run --volume ${HOME}/.m2/repository:/root/.m2/repository \
+             --volume ${HOME}/.npm:/root/.npm \
+             --volume $(pwd):/root/visallo \
+             -w /root/visallo \
+             -e "MVN_REPO_USERNAME=${DEPLOY_USERNAME}" \
+             -e "MVN_REPO_PASSWORD=${DEPLOY_PASSWORD}" \
+             --rm -it visallo/travis:visallo-4.0.0 \
+             /bin/sh -c "mvn -B -f root/pom.xml deploy && mvn -B -DskipTests deploy"
+      fi
+    fi
+  fi
+
+  CREATED_BY="${TRAVIS_REPO_SLUG} commit "`git rev-parse --short HEAD`""
+  if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then ./travis/travis-request.sh --repo ${TRAVIS_DOWNSTREAM_REPO} --token ${TRAVIS_ACCESS_TOKEN_PRO} --by "${CREATED_BY}" --branch ${TRAVIS_BRANCH}; fi
 fi
--- travis/script.sh
+++ travis/script.sh
@@ -1,22 +1,31 @@
-#!/bin/bash
+#!/bin/bash -e
 
-set -e
-
-if [ "$BUILD_DOCS" ]; then
+if [ "${BUILD_DOCS}" ]; then
 
   # only build if merging into real branch
-  if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 
     # Check if this branch should be built as part of documentation
-    if [[ $VERSION_LIST =~ (^|[[:space:]])$TRAVIS_BRANCH($|[[:space:]]) ]]; then
-      source ~/.nvm/nvm.sh
-      VERSION_CURRENT="$TRAVIS_BRANCH" make -C docs link-check-external
+    if [[ ${VERSION_LIST} =~ (^|[[:space:]])${TRAVIS_BRANCH}($|[[:space:]]) ]]; then
+      docker run --volume ${HOME}/.m2/repository:/root/.m2/repository \
+             --volume ${HOME}/.npm:/root/.npm \
+             --volume $(pwd):/root/visallo \
+             -w /root/visallo \
+             -e "VERSION_CURRENT=${TRAVIS_BRANCH}" \
+             --rm -it visallo/travis:visallo-4.0.0 \
+             /bin/sh -c "make -C docs link-check-external"
     else
       echo "Branch not found in VERSION_LIST for docs, skipping"
-      rm -rf docs/_book
     fi
   fi
 
 else
-  mvn -B -fae test -Ptest-exclude-native -DlogQuiet
+  docker run --volume ${HOME}/.m2/repository:/root/.m2/repository \
+             --volume ${HOME}/.npm:/root/.npm \
+             --volume $(pwd):/root/visallo \
+             -w /root/visallo \
+             -e "MVN_REPO_USERNAME=${DEPLOY_USERNAME}" \
+             -e "MVN_REPO_PASSWORD=${DEPLOY_PASSWORD}" \
+             --rm -it visallo/travis:visallo-4.0.0 \
+             /bin/sh -c "mvn -B -fae test -DlogQuiet"
 fi
