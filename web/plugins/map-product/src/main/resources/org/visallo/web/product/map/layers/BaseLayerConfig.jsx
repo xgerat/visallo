9aa773dd68c6fa300fbad3f5586f16e3f42737a0
Joey Burke <joeybrk372@gmail.com>
2018-02-02 11:30:24 -0500

    Cleanup BaseLayerConfig.jsx

--- web/plugins/map-product/src/main/resources/org/visallo/web/product/map/layers/BaseLayerConfig.jsx
+++ web/plugins/map-product/src/main/resources/org/visallo/web/product/map/layers/BaseLayerConfig.jsx
@@ -1,21 +1,20 @@
 define([
     'prop-types',
-    'create-react-class',
-    'classnames',
+    'create-react-class'
 ], function(
     PropTypes,
     createReactClass) {
 
     const BaseLayerConfig = createReactClass({
         propTypes: {
-            // layer: PropTypes.object.isRequired,
-            // config: PropTypes.object,
-            // onUpdateLayerConfig: PropTypes.func
+            layer: PropTypes.object.isRequired,
+            config: PropTypes.object,
+            onUpdateLayerConfig: PropTypes.func.isRequired
         },
 
         render() {
             return (
-                <div className="base-layer-provider">
+                <div className={'base-layer-provider'}>
                     <button onClick={this.onToggleProvider}>Toggle Provider</button>
                 </div>
             )
@@ -23,13 +22,9 @@ define([
 
         onToggleProvider() {
             const { layer, config = {}, onUpdateLayerConfig } = this.props;
-            if (layer.get('id') === 'base') {
-                const { provider: prevProvider, ...rest } = config;
+            let provider = !config.provider || config.provider === 'osm' ? 'BingMaps' : 'osm';
 
-                let provider = !prevProvider || prevProvider === 'osm' ? 'BingMaps' : 'osm';
-
-                onUpdateLayerConfig({ ...config, provider }, layer);
-            }
+            onUpdateLayerConfig({ ...config, provider }, layer);
         }
     });
 
