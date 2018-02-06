1669f7ec4a17ea1b20ece286b466593cd1c758e5
Jason Harwig <jason.harwig@gmail.com>
2018-02-06 09:20:07 -0600

    remove log

--- web/plugins/graph-product/src/main/resources/org/visallo/web/product/graph/layouts/circle.js
+++ web/plugins/graph-product/src/main/resources/org/visallo/web/product/graph/layouts/circle.js
@@ -3,8 +3,7 @@ define(['./circleSimple', './circleConcentric'], function(simple, concentric) {
     return function circleLayout(options = {}) {
         const { eles } = options;
         const nodes = eles.filter('node');
-
-        console.log('nodes', nodes.length, nodes.maxDegree())
+        
         if (nodes.maxDegree() === 0) {
             return simple.call(this, options);
         } else {
